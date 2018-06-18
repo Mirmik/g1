16e9c80e29e0b3aa3a2e2ca7ca35b913af595c17
mirmik <mirmikns@yandex.ru>
2018-06-18 19:41:51 +0300

    a

--- g1/src/stdtime.cpp
+++ g1/src/stdtime.cpp
@@ -4,4 +4,5 @@
 uint16_t g1::millis() {
 	auto duration = std::chrono::system_clock::now().time_since_epoch();
 	auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+	return millis;
 }
